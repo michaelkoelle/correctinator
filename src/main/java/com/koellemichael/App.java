8d8db4dae4c0e0b1355cefe7135e12cc7c3f8997
Michael Kölle <koelle.michael@gmail.com>
2019-06-21 23:01:01 +0200

    added scaling not fits screen size

--- src/main/java/com/koellemichael/App.java
+++ src/main/java/com/koellemichael/App.java
@@ -3,14 +3,21 @@ package com.koellemichael;
 import com.koellemichael.controller.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        double width = primaryScreenBounds.getWidth() / 1.2;
+        double height = primaryScreenBounds.getHeight() / 1.2;
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/layout/sample.fxml"));
         Parent root = loader.load();
         Controller controller = loader.getController();
@@ -18,7 +25,7 @@ public class App extends Application {
 
         primaryStage.setTitle("Correctinator");
         primaryStage.setFullScreenExitHint("F1 um Vollbild zu verlassen");
-        primaryStage.setScene(new Scene(root, 1300, 800));
+        primaryStage.setScene(new Scene(root, width, height));
         primaryStage.show();
     }
 
