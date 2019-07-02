52fe7001323ae11e354c8ba985ae453fd6515a79
Michael Kölle <koelle.michael@gmail.com>
2019-07-02 14:13:51 +0200

    added version number to title

--- src/main/java/com/koellemichael/App.java
+++ src/main/java/com/koellemichael/App.java
@@ -8,25 +8,28 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        /*
+        primaryStage.setTitle("Correctinator");
+
         MavenXpp3Reader reader = new MavenXpp3Reader();
         Model model;
-        if ((new File("pom.xml")).exists())
+
+        if ((new File("pom.xml")).exists()){
             model = reader.read(new FileReader("pom.xml"));
-        else
-            model = reader.read(
-                    new InputStreamReader(
-                            Application.class.getResourceAsStream(
-                                    "/META-INF/maven/de.scrum-master.stackoverflow/aspectj-introduce-method/pom.xml"
-                            )
-                    )
-            );
-        */
+            primaryStage.setTitle("Correctinator " + model.getVersion());
+            System.out.println(model.getVersion());
+        }
+
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
         double width = primaryScreenBounds.getWidth() / 1.2;
         double height = primaryScreenBounds.getHeight() / 1.2;
@@ -36,7 +39,7 @@ public class App extends Application {
         Controller controller = loader.getController();
         controller.initialize(primaryStage);
 
-        primaryStage.setTitle("Correctinator");
+
         primaryStage.setFullScreenExitHint("F1 um Vollbild zu verlassen");
         primaryStage.setScene(new Scene(root, width, height));
         primaryStage.show();
