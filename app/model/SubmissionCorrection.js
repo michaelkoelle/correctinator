e3c9f55ce37d0f0efa1a7553e40f02f0356e4e09
Michael Kölle <koelle.michael@gmail.com>
2019-09-22 23:16:07 +0200

    added exercise reference

--- app/model/SubmissionCorrection.js
+++ app/model/SubmissionCorrection.js
@@ -2,7 +2,7 @@ import Submission from './Submission';
 
 export default class SubmissionCorrection extends Submission {
   constructor(state, annotation, ratingFilePath, filePaths, exercise, ratings) {
-    super(SubmissionCorrection.incrementId(), filePaths);
+    super(SubmissionCorrection.incrementId(), filePaths, exercise);
     this.state = state;
     this.annotation = annotation;
     this.ratingFilePath = ratingFilePath;
