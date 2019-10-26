7020160b27f89206d8a366c7faf10480f0dfae0c
Michael Kölle <koelle.michael@gmail.com>
2019-10-26 19:15:11 +0200

    added debug for pdf viewer

--- src/main/java/com/koellemichael/controller/PDFViewController.java
+++ src/main/java/com/koellemichael/controller/PDFViewController.java
@@ -1,23 +1,28 @@
 package com.koellemichael.controller;
 
+import com.koellemichael.utils.PreferenceKeys;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
+import javafx.scene.control.Alert;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Locale;
+import java.util.prefs.Preferences;
 
 public class PDFViewController implements ChangeListener {
 
     public WebView web_pdf;
     private WebEngine engine;
     private File pdf;
+    private Preferences preferences;
 
     public void initialize(File pdf){
         this.pdf = pdf;
@@ -27,6 +32,8 @@ public class PDFViewController implements ChangeListener {
         engine.setUserStyleSheetLocation(getClass().getResource("/pdfviewer/web/viewer.css").toExternalForm());
         engine.load(getClass().getResource("/pdfviewer/web/viewer.html").toExternalForm());
         engine.getLoadWorker().stateProperty().addListener(this);
+
+        preferences = Preferences.userRoot();
     }
 
     private void loadPDF(File pdf){
@@ -35,6 +42,17 @@ public class PDFViewController implements ChangeListener {
                 byte[] data = Files.readAllBytes(Paths.get(pdf.getAbsolutePath()));
                 String base64 = Base64.getEncoder().encodeToString(data);
                 engine.executeScript("openFileFromBase64('" + base64 + "')");
+
+                if(preferences.getBoolean(PreferenceKeys.VERBOSE_PREF,false)){
+                    Alert deb = new Alert(Alert.AlertType.INFORMATION);
+                    deb.setTitle("DEBUG");
+                    deb.setHeaderText(null);
+                    String sb = "Absoluter PDF Pfad: " + pdf.getAbsolutePath() + "\n" +
+                                "Relativer PDF Pfad: " + pdf.getPath() + "\n" +
+                                "Paths: " + Paths.get(pdf.getAbsolutePath()) + "\n";
+                    deb.setContentText(sb);
+                    deb.showAndWait();
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
