7622bfb26a4c9324d33227e255480ce09170c02e
Michael Kölle <koelle.michael@gmail.com>
2020-10-28 13:49:27 +0100

    feat: Time Display now shows zero when undefined

--- app/components/TimeElapsedDisplay.tsx
+++ app/components/TimeElapsedDisplay.tsx
@@ -1,38 +1,53 @@
 import { Tooltip, Typography } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 
+function formatMillis(millis: number | undefined): string {
+  let diff = 0;
+
+  if (millis) {
+    diff = millis;
+  }
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  diff -= days * (1000 * 60 * 60 * 24);
+
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  diff -= hours * (1000 * 60 * 60);
+
+  const mins = Math.floor(diff / (1000 * 60));
+  diff -= mins * (1000 * 60);
+
+  const seconds = Math.floor(diff / 1000);
+  diff -= seconds * 1000;
+
+  return `${hours < 10 ? `0${hours}` : hours}:${
+    mins < 10 ? `0${mins}` : mins
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function formatDifference(
+  date1: Date | undefined,
+  date2: Date | undefined,
+  elapsed: number | undefined
+): string {
+  const temp: number = elapsed || 0;
+
+  let millis = temp;
+  if (date1 !== undefined && date2 !== undefined) {
+    millis = date2.getTime() - date1.getTime() + temp;
+  }
+  return formatMillis(millis);
+}
+
 export default function TimeElapsedDisplay(props: {
   start: Date | undefined;
   elapsed: number | undefined;
 }) {
   const { start, elapsed } = props;
-  const [time, setTime] = useState<string>('');
-
-  function formatDifference(date1: Date, date2: Date): string {
-    const temp: number = elapsed || 0;
-    let diff = date2.getTime() - date1.getTime() + temp;
-
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    diff -= days * (1000 * 60 * 60 * 24);
-
-    const hours = Math.floor(diff / (1000 * 60 * 60));
-    diff -= hours * (1000 * 60 * 60);
-
-    const mins = Math.floor(diff / (1000 * 60));
-    diff -= mins * (1000 * 60);
-
-    const seconds = Math.floor(diff / 1000);
-    diff -= seconds * 1000;
-
-    return `${hours < 10 ? `0${hours}` : hours}:${
-      mins < 10 ? `0${mins}` : mins
-    }:${seconds < 10 ? `0${seconds}` : seconds}`;
-  }
+  const [time, setTime] = useState<string>(formatMillis(elapsed));
 
   function tick() {
-    if (start) {
-      setTime(formatDifference(start, new Date()));
-    }
+    setTime(formatDifference(start, new Date(), elapsed));
   }
 
   useEffect(() => {
