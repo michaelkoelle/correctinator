ca153d22cab644532b8a6506053788e9a304dd7f
Michael Kölle <koelle.michael@gmail.com>
2021-01-26 04:18:41 +0100

    feat: Added Checklist, SingleChice, MultipleChoice Tasks and Ratings

--- app/model/Rating.tsx
+++ app/model/Rating.tsx
@@ -1,5 +1,16 @@
-import Task from './Task';
 import Comment from './Comment';
+import { Task } from './Task';
 
-type Rating = { id: string; value: number; comment: Comment; task: Task };
+type BaseRating = { id: string; comment: Comment; task: Task };
+
+export type ValueRating = BaseRating & {
+  value: number;
+};
+
+export type MultipleValueRating = {
+  values: number[];
+};
+
+export type Rating = ValueRating | MultipleValueRating;
+
 export default Rating;
--- app/model/Task.tsx
+++ app/model/Task.tsx
@@ -1,8 +1,39 @@
-type Task = {
+type BaseTask = {
   id: string;
   name: string;
-  step?: number;
-  max?: number;
-  tasks?: Task[];
 };
-export default Task;
+
+export type ParentTask = BaseTask & {
+  tasks: Task[];
+};
+
+export type RateableTask = BaseTask & {
+  step: number;
+  max: number;
+};
+
+export type SingleChoiceTask = RateableTask & {
+  options: string[];
+  answer: number;
+};
+
+export type MultipleChoiceTask = RateableTask & {
+  options: string[];
+  answer: number[];
+};
+
+export type ChecklistItem = {
+  text: string;
+  value: number;
+};
+
+export type ChecklistTask = RateableTask & {
+  checklist: ChecklistItem[];
+};
+
+export type Task =
+  | ParentTask
+  | RateableTask
+  | SingleChoiceTask
+  | MultipleChoiceTask
+  | ChecklistTask;
