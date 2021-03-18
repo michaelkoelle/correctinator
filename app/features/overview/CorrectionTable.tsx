4f8f6aab4373da4c383baa9a6ac4a1926734adbf
Michael Kölle <koelle.michael@gmail.com>
2021-03-18 23:24:23 +0100

    feat: Added enhanced datatable to overview

--- app/features/overview/CorrectionTable.tsx
+++ app/features/overview/CorrectionTable.tsx
@@ -1,56 +1,44 @@
 import {
-  TableContainer,
-  Paper,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
   useTheme,
+  ThemeProvider,
+  createMuiTheme,
+  IconButton,
 } from '@material-ui/core';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import React from 'react';
+import MUIDataTable, { MUIDataTableColumnDef } from 'mui-datatables';
 import { useDispatch } from 'react-redux';
 import StatusIcon from '../../components/StatusIcon';
 import Correction from '../../model/Correction';
 import {
-  correctionPageSetIndex,
   correctionPageSetIndexOfCorrection,
   correctionPageSetSheetId,
 } from '../../model/CorrectionPageSlice';
 import { setTabIndex } from '../../model/HomeSlice';
+import Status, { statusToString } from '../../model/Status';
 import { getTotalValueOfRatings } from '../../utils/Formatter';
 import { msToTime } from '../../utils/TimeUtil';
 
 type CorrectionTableProps = {
+  title: string;
   corrections: Correction[];
 };
 
+declare module '@material-ui/core/styles/overrides' {
+  interface ComponentNameToClassKey {
+    MUIDataTableHeadCell?: {
+      toolButton: {
+        marginLeft?: string;
+        marginRight?: string;
+      };
+    };
+  }
+}
+
 export default function CorrectionTable(props: CorrectionTableProps) {
-  const { corrections } = props;
+  const { corrections, title } = props;
   const theme = useTheme();
   const dispatch = useDispatch();
-
-  const rows = corrections
-    ? corrections.map((c) => {
-        return {
-          status: c.status,
-          matNr: c.submission.matNr ? c.submission.matNr : 'anonymous',
-          name: c.submission.name,
-          sheetId: c.submission.sheet.id,
-          sheet: c.submission.sheet.name,
-          course: c.submission.sheet.course.name,
-          term: `${c.submission.sheet.term.summerterm ? 'SS' : 'WS'}${
-            c.submission.sheet.term.year
-          }`,
-          rating: c.ratings ? getTotalValueOfRatings(c.ratings) : 0,
-          max: c.submission.sheet.maxValue,
-          valueType: c.submission.sheet.valueType,
-          time: c.timeElapsed ? c.timeElapsed : 0,
-          note: c.note ? c.note.text : '',
-          correctionId: c.id,
-        };
-      })
-    : [];
 
   function openCorrection(sheetId: string, correctionId: string) {
     dispatch(correctionPageSetSheetId(sheetId));
@@ -58,93 +46,184 @@ export default function CorrectionTable(props: CorrectionTableProps) {
     dispatch(setTabIndex(3));
   }
 
+  const getMuiTheme = () =>
+    createMuiTheme(
+      {
+        overrides: {
+          MuiPaper: {
+            root: {
+              // height: 'calc(100% - 20px)',
+              height: 'inherit',
+            },
+          },
+          MuiTable: {
+            root: {
+              userSelect: 'none',
+            },
+          },
+          MuiTableCell: {
+            sizeSmall: {
+              padding: '12px 12px 12px 12px',
+            },
+          },
+          MuiButton: {
+            text: {
+              padding: '0px',
+            },
+          },
+          MUIDataTableHeadCell: {
+            toolButton: {
+              marginLeft: '0px',
+              marginRight: '0px',
+            },
+          },
+          MuiToolbar: { root: { minHeight: '50px' } },
+        },
+      },
+      theme
+    );
+
+  const columns: MUIDataTableColumnDef[] = [
+    {
+      name: 'status',
+      label: 'Status',
+      options: {
+        filterOptions: {
+          renderValue: (value) => statusToString(parseInt(value, 10) as Status),
+        },
+        customFilterListOptions: {
+          render: (value) => statusToString(parseInt(value, 10) as Status),
+        },
+        // eslint-disable-next-line react/display-name
+        customBodyRender: (value) => {
+          return <StatusIcon status={parseInt(value, 10) as Status} />;
+        },
+      },
+    },
+    {
+      name: 'matNr',
+      label: 'MatNr',
+    },
+    {
+      name: 'name',
+      label: 'Submission ID',
+    },
+    {
+      name: 'course',
+      label: 'Course',
+    },
+    {
+      name: 'term',
+      label: 'Term',
+    },
+    {
+      name: 'sheet',
+      label: 'Sheet',
+    },
+    {
+      name: 'rating',
+      label: 'Rating',
+    },
+    {
+      name: 'time',
+      label: 'Time',
+    },
+    {
+      name: 'note',
+      label: 'Note',
+    },
+    {
+      name: '',
+      label: 'Go to',
+      options: {
+        filter: false,
+        // eslint-disable-next-line react/display-name
+        customBodyRender: (_value, tableMeta) => {
+          return (
+            <IconButton
+              size="small"
+              onClick={() => {
+                const index = tableMeta.rowIndex;
+                if (corrections[index]) {
+                  openCorrection(
+                    corrections[index].submission.sheet.id,
+                    corrections[index].id
+                  );
+                }
+              }}
+            >
+              <ExitToAppIcon />
+            </IconButton>
+          );
+        },
+      },
+    },
+  ];
+
   return (
-    <TableContainer
-      component={Paper}
-      style={{ height: 'inherit', userSelect: 'none' }}
-    >
-      <Table stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableCell
-              align="center"
-              style={{ backgroundColor: theme.palette.background.paper }}
-            >
-              Status
-            </TableCell>
-            <TableCell
-              style={{ backgroundColor: theme.palette.background.paper }}
-            >
-              MatNr
-            </TableCell>
-            <TableCell
-              style={{ backgroundColor: theme.palette.background.paper }}
-            >
-              Name
-            </TableCell>
-            <TableCell
-              style={{ backgroundColor: theme.palette.background.paper }}
-            >
-              Course
-            </TableCell>
-            <TableCell
-              style={{ backgroundColor: theme.palette.background.paper }}
-            >
-              Term
-            </TableCell>
-            <TableCell
-              style={{ backgroundColor: theme.palette.background.paper }}
-            >
-              Sheet
-            </TableCell>
-            <TableCell
-              style={{ backgroundColor: theme.palette.background.paper }}
-            >
-              Rating
-            </TableCell>
-            <TableCell
-              style={{ backgroundColor: theme.palette.background.paper }}
-            >
-              Time
-            </TableCell>
-            <TableCell
-              style={{ backgroundColor: theme.palette.background.paper }}
-            >
-              Note
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows
-            .sort(
-              (el, next) => parseInt(el.matNr, 10) - parseInt(next.matNr, 10)
-            )
-            .map((row) => (
-              <TableRow
-                key={row.name}
-                onDoubleClick={() => {
-                  openCorrection(row.sheetId, row.correctionId);
-                }}
-              >
-                <TableCell align="center">
-                  <StatusIcon status={row.status} />
-                </TableCell>
-                <TableCell>{row.matNr}</TableCell>
-                <TableCell>{row.name}</TableCell>
-                <TableCell>{row.course}</TableCell>
-                <TableCell>{row.term}</TableCell>
-                <TableCell>{row.sheet}</TableCell>
-                <TableCell>{`${row.rating} / ${row.max}`}</TableCell>
-                <TableCell>
-                  {row.time > 60 * 60 * 1000
-                    ? msToTime(row.time)
-                    : msToTime(row.time).substring(3)}
-                </TableCell>
-                <TableCell>{row.note}</TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <ThemeProvider theme={getMuiTheme()}>
+      <MUIDataTable
+        title={title}
+        data={
+          corrections
+            ? corrections.map((c) => {
+                return {
+                  status: c.status,
+                  matNr: c.submission.matNr ? c.submission.matNr : 'anonymous',
+                  name: c.submission.name,
+                  sheetId: c.submission.sheet.id,
+                  sheet: c.submission.sheet.name,
+                  course: c.submission.sheet.course.name,
+                  term: `${c.submission.sheet.term.summerterm ? 'SS' : 'WS'}${
+                    c.submission.sheet.term.year
+                  }`,
+                  rating: `${
+                    c.ratings ? getTotalValueOfRatings(c.ratings) : 0
+                  } / ${c.submission.sheet.maxValue}`,
+                  max: c.submission.sheet.maxValue,
+                  valueType: c.submission.sheet.valueType,
+                  // eslint-disable-next-line no-nested-ternary
+                  time: c.timeElapsed
+                    ? c.timeElapsed > 60 * 60 * 1000
+                      ? msToTime(c.timeElapsed)
+                      : msToTime(c.timeElapsed).substring(3)
+                    : 'N/A',
+                  note: c.note ? c.note.text : '',
+                  correctionId: c.id,
+                };
+              })
+            : []
+        }
+        columns={columns}
+        options={{
+          enableNestedDataAccess: '.',
+          print: false,
+          pagination: false,
+          // selectableRows: 'muliple',
+          selectableRowsHideCheckboxes: true,
+          selectableRowsOnClick: false,
+          selectToolbarPlacement: 'none',
+          responsive: 'simple',
+          tableBodyHeight: 'calc(100% - 65px)',
+          // onRowSelectionChange: onSelectionChange,
+          setTableProps: () => {
+            return {
+              // padding: 'none',
+              size: 'small',
+            };
+          },
+          setRowProps: (_row, index) => ({
+            onDoubleClick: () => {
+              if (corrections[index]) {
+                openCorrection(
+                  corrections[index].submission.sheet.id,
+                  corrections[index].id
+                );
+              }
+            },
+          }),
+        }}
+      />
+    </ThemeProvider>
   );
 }
