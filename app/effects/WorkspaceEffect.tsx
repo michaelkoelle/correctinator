8f021d4655030abd8020f76b3692dba89925095c
Michael Kölle <koelle.michael@gmail.com>
2021-11-09 13:57:16 +0100

    fix: Fixed state reset on app start if reference to .cor file is lost

--- app/effects/WorkspaceEffect.tsx
+++ app/effects/WorkspaceEffect.tsx
@@ -5,12 +5,22 @@ import { reloadState } from '../utils/FileAccess';
 
 const WorkspaceEffect = (dispatch, workspacePath: string): EffectCallback => {
   return () => {
+    const resetState = () => {
+      dispatch(workspaceSetPath(''));
+      dispatch(reloadState());
+    };
+
     const handleChange = (curr: fs.Stats, prev: fs.Stats) => {
       if (!curr.isFile() && prev.isFile()) {
-        dispatch(workspaceSetPath(''));
-        dispatch(reloadState());
+        resetState();
       }
     };
+
+    // Check if workspace file exists
+    if (!fs.existsSync(workspacePath) || !fs.statSync(workspacePath).isFile()) {
+      resetState();
+    }
+
     // Start watching
     fs.watchFile(workspacePath, handleChange);
     return () => {
