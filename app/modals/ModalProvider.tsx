72c1f92cc6538937991458d27c300768218566bb
Michael Kölle <koelle.michael@gmail.com>
2021-12-18 17:09:58 +0100

    feat: ModalProvider is now able to show more than one modal at a time

--- app/modals/ModalProvider.tsx
+++ app/modals/ModalProvider.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/jsx-pascal-case */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { createContext, FC, useContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 interface ModalProviderProps {
   children;
@@ -10,35 +12,44 @@ export interface ModalProps {
   close: () => void;
 }
 
+interface ModalConfig<T> {
+  options: T;
+  component: FC<T & ModalProps>;
+}
+
 const ModalContext = createContext<
   <T>(component: FC<T & ModalProps>, options?: T) => void
 >(() => undefined);
 
 export default function ModalProvider(props: ModalProviderProps) {
   const { children } = props;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [Modal, setModal] = useState<any>(null);
-  const [modalOptions, setModalOptions] = useState<unknown>(null);
+  const [modalList, setModalList] = useState<ModalConfig<unknown>[]>([]);
 
   const openModal = <T extends unknown>(
     component: FC<T & ModalProps>,
     options: T
   ) => {
-    setModalOptions(options);
-    setModal(() => component);
+    setModalList([
+      ...modalList,
+      { options, component: component as FC<ModalProps> },
+    ]);
   };
 
-  const closeModal = () => {
-    setModal(null);
-    setModalOptions(null);
+  const closeModal = (index: number) => {
+    setModalList(modalList.filter((_, i) => index !== i));
   };
 
   return (
     <ModalContext.Provider value={openModal}>
       {children}
-      {Modal !== null && modalOptions !== null && (
-        <Modal open={Boolean(Modal)} close={closeModal} {...modalOptions} />
-      )}
+      {modalList.map((Modal, i) => (
+        <Modal.component
+          key={uuidv4()}
+          open={Boolean(Modal.component)}
+          close={() => closeModal(i)}
+          {...Modal.options}
+        />
+      ))}
     </ModalContext.Provider>
   );
 }
